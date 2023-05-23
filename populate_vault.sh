31f7946034437779d91cdba9e5913ef62c2858a0
Jacob Kaae <jacobkaae0511@gmail.com>
2023-05-23 08:46:31 +0200

    added nginx

--- populate_vault.sh
+++ populate_vault.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export VAULT_ADDR='https://vault:8201'
+export VAULT_ADDR='http://jacob:8200'
 export VAULT_TOKEN='00000000-0000-0000-0000-000000000000'
 
 vault kv put secret/hemmeligheder MinKode=abcd1234
 vault kv put secret/connectionStrings ConnectionURI=mongodb://admin:1234@mongodb-dev:27017/?authSource=admin
-vault kv put secret/environmentVariables Secret=fwnhy8423HBgbirfffwefefwefwefwedqwsad6q3wfrhgedr32etsg7u Issuer=JADADADDAADA Salt='$2a$11$NnQ3D9KHpPr1UOjTo/2fXO'
+vault kv put secret/enviromentVariables Secret=fwnhy8423HBgbirfffwefefwefwefwedqwsad6q3wfrhgedr32etsg7u Issuer=JADADADDAADA Salt='$2a$11$NnQ3D9KHpPr1UOjTo/2fXO'
                                
 
 
