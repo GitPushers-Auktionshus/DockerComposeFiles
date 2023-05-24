524912aca8363d7cb0ef5f4709888720e79731b2
Anders <90785699+lynge96@users.noreply.github.com>
2023-05-24 09:33:57 +0200

    +n :')

--- fill.sh
+++ fill.sh
@@ -1,5 +1,5 @@
 # #!/bin/sh
 echo "[*] Running fill.sh script"
 vault kv put secret/connectionStrings ConnectionURI=mongodb://admin:1234@mongodb-dev:27017/?authSource=admin
-vault kv put secret/enviromentVariables Secret=fwnhy8423HBgbirfffwefefwefwefwedqwsad6q3wfrhgedr32etsg7u Issuer=JADADADDAADA Salt='$2a$11$NnQ3D9KHpPr1UOjTo/2fXO'
+vault kv put secret/environmentVariables Secret=fwnhy8423HBgbirfffwefefwefwefwedqwsad6q3wfrhgedr32etsg7u Issuer=JADADADDAADA Salt='$2a$11$NnQ3D9KHpPr1UOjTo/2fXO'
 vault kv get -mount=secret connectionStrings
