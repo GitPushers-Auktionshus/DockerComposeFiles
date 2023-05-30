b91f6154daa0f5462aa449074eb25d9535bf8988
Jacob Kaae <jacobkaae0511@gmail.com>
2023-05-30 11:26:23 +0200

    Changed populatevualt

--- populate_vault.sh
+++ populate_vault.sh
@@ -3,7 +3,6 @@
 export VAULT_ADDR='http://jacob:8200'
 export VAULT_TOKEN='00000000-0000-0000-0000-000000000000'
 
-vault kv put secret/hemmeligheder MinKode=abcd1234
 vault kv put secret/connectionStrings ConnectionURI=mongodb://admin:1234@mongodb-dev:27017/?authSource=admin
 vault kv put secret/environmentVariables Secret=fwnhy8423HBgbirfffwefefwefwefwedqwsad6q3wfrhgedr32etsg7u Issuer=JADADADDAADA Salt='$2a$11$NnQ3D9KHpPr1UOjTo/2fXO'
                                
